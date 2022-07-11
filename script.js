c3d678bb700de5d2b47af6de033b4901b13c16c9
truongzodinet <truong.tran@zodinet.tech>
2022-07-11 16:34:27 +0700

    fixes params type

--- script.js
+++ script.js
@@ -5,24 +5,23 @@ const button = document.querySelector("#btn");
 const display = document.querySelector("#result");
 
 function getResult(a, b, c) {
-    let delta = Math.pow(b) - 4 * a * c;
+    let delta = Math.pow(b, 2) - 4*a*c;
     let result = "";
 
     if (delta < 0) {
         result = 'No solution';
     }
     if (delta === 0) {
-        result = 'x1 = x2 = ' + (-b / (2 * a)).toString();
+        result = 'X1 = X2 = ' + (-b / (2 * a)).toString();
     }
     if (delta > 0) {
-        result = 'x1 = ' + ((-b + Math.sqrt(delta)) / (2 * a)).toString() + ' and x2 = ' + ((-b - Math.sqrt(delta)) / (2 * a)).toString();
+        result = 'X1 = ' + ((-b + Math.sqrt(delta)) / (2 * a)).toString() + ' and X2 = ' + ((-b - Math.sqrt(delta)) / (2 * a)).toString();
     }
     return result;
 }
 
 button.addEventListener("click", () => {
-    console.log('oke');
     if (first_number.value && second_number.value && third_number.value) {
-        display.innerText = getResult(parseFloat(first_number.value), parseFloat(second_number.value), parseFloat(third_number.value));
+        display.innerText = 'Result: ' + getResult(parseFloat(first_number.value), parseFloat(second_number.value), parseFloat(third_number.value));
     }
 });
