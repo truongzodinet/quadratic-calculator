80f7eeb65c0d5e42dae15c66f7422c4a72a9853d
truongzodinet <truong.tran@zodinet.tech>
2022-07-12 09:17:29 +0700

    fix: handle logic when a=0

--- script.js
+++ script.js
@@ -4,25 +4,31 @@ let third_number = document.querySelector("#third_number");
 const button = document.querySelector("#btn");
 const display = document.querySelector("#result");
 
-const reset = () =>{
+const reset = () => {
     first_number.value = "";
     second_number.value = "";
     third_number.value = "";
-    display.innerHTML = "";
+    display.innerText = "No have result: ";
 }
 
 const getResult = (a, b, c) => {
     let delta = Math.pow(b, 2) - 4 * a * c;
     let result = "";
 
-    if (delta < 0) {
-        result = 'No solution';
+    if (a === 0) {
+        result = 'No solution, no root';
     }
-    if (delta === 0) {
-        result = 'X1 = X2 = ' + (-b / (2 * a)).toString();
-    }
-    if (delta > 0) {
-        result = 'X1 = ' + ((-b + Math.sqrt(delta)) / (2 * a)).toString() + ' and X2 = ' + ((-b - Math.sqrt(delta)) / (2 * a)).toString();
+
+    else{
+        if (delta < 0) {
+            result = 'The equation no have solution';
+        }
+        if (delta === 0) {
+            result = `The equation have double root: X1 = X2 = ${(-b / (2 * a)).toString()}`;
+        }
+        if (delta > 0) {
+            result = `The equation have two root: X1 = ${((-b + Math.sqrt(delta)) / (2 * a)).toString()} and X2 = ${((-b - Math.sqrt(delta)) / (2 * a)).toString()}`;
+        }
     }
     return result;
 }
@@ -30,8 +36,7 @@ const getResult = (a, b, c) => {
 button.addEventListener("click", () => {
     if (first_number.value && second_number.value && third_number.value) {
         display.innerText = 'Result: ' + getResult(parseFloat(first_number.value), parseFloat(second_number.value), parseFloat(third_number.value));
-    }
-    else{
+    } else {
         alert("Please fill all fields");
     }
 });
